6dd554115e81d6bdea954d48576b9bd0d19160c7
Franze Jr <franzejr@gmail.com>
2015-09-29 07:29:15 -0300

    Changing Client module and fixing imports

--- lib/hackerex/users.ex
+++ lib/hackerex/users.ex
@@ -1,8 +1,10 @@
 defmodule  Hackerex.Users do
   import Hackerex
-  import Hackerex.Client
+  alias Hackerex.Client
+
   @spec find(binary, Client.t) :: Hackerex.response
   def find(user, client \\ %Client{}) do
     get "user/#{user}.json", client
   end
+
 end
